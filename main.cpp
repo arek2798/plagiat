4616716a85584b7e728135bff752a9dd3d8a2eeb
arkad.kuptz <arekk2798@outlook.com>
2020-06-01 23:49:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-#define MAX_ROZMIAR 100 ///ilosc znakow jaka bedzie wczytywana do pamieci
+#define MAX_ROZMIAR 100
 
-int utworzenie_tablicy_ascii(ifstream& plik, int tablica[MAX_ROZMIAR]) { ///tworzenie tablicy liczb na podstawie znakow zczytanych z plikow
+int utworzenie_tablicy_ascii(ifstream& plik, int tablica[MAX_ROZMIAR]) { 
     char znak;
     int ile=0;
 
-    while( ile < MAX_ROZMIAR && plik.get(znak)) { ///konczy sie gdy dojdziemy do konca pliku albo osiagniemy maksywalna ilosc wczytanych znakow
-        if(!isspace(znak)) { ///zamieniamy tylko wtedy gdy nie jest znakiem bialym jesli jest to pomijamy i przechodzimy do nastepnego
+    while( ile < MAX_ROZMIAR && plik.get(znak)) {
+        if(!isspace(znak)) { 
             znak = tolower(znak);
             tablica[ile] = static_cast<int>(znak);
             ile++;
@@ -20,14 +20,14 @@ int utworzenie_tablicy_ascii(ifstream& plik, int tablica[MAX_ROZMIAR]) { ///twor
     return ile;
 }
 
-float srednia_ciagu(ifstream& plik, int& suma_pliku, int& dlugosc_pliku) { ///liczenie wartosci sredniej ciagu liczb utworzonych na podstawie znakow
+float srednia_ciagu(ifstream& plik, int& suma_pliku, int& dlugosc_pliku) {
     int suma = 0, ile = 0;
     char znak;
 
     while(plik.get(znak)) {
         if(!isspace(znak)) {
-            znak = tolower(znak); ///zamiana wszystkich liter na male
-            suma += static_cast<int>(znak); ///dodanie do sumy liczby ktora odpowiada danej literze
+            znak = tolower(znak);
+            suma += static_cast<int>(znak);
             ile++;
         }
     }
@@ -36,7 +36,7 @@ float srednia_ciagu(ifstream& plik, int& suma_pliku, int& dlugosc_pliku) { ///li
     return suma/ile;
 }
 
-float licz_licznik(int x[MAX_ROZMIAR], int y[MAX_ROZMIAR], float sr_x, float sr_y, int ile) { ///obliczenie wartosci licznika w rownaniu na wspolczynnik korelacji
+float licz_licznik(int x[MAX_ROZMIAR], int y[MAX_ROZMIAR], float sr_x, float sr_y, int ile) {
     float suma=0;
     for(int i=0; i<ile; i++) {
         suma += (x[i]-sr_x) * (y[i]-sr_y);
@@ -44,7 +44,7 @@ float licz_licznik(int x[MAX_ROZMIAR], int y[MAX_ROZMIAR], float sr_x, float sr_
     return suma;
 }
 
-float licz_mianownik(int tablica[MAX_ROZMIAR], int srednia, int ile) { ///obliczenie wartosci mianownika w rownaniu na wspolczynnik korelacji
+float licz_mianownik(int tablica[MAX_ROZMIAR], int srednia, int ile) { 
     float suma=0;
     for(int i=0; i<ile; i++) {
         suma += (tablica[i]-srednia)*(tablica[i]-srednia);
@@ -67,39 +67,39 @@ float porownanie_plikow(ifstream& plik_1, ifstream& plik_2) {
     float wspolczynnik, sr_x, sr_y, licznik = 0, mianownik_1 = 0, mianownik_2 = 0;
     int dlugosc_plik_1, dlugosc_plik_2, suma_plik_1, suma_plik_2;
 
-    sr_x = srednia_ciagu(plik_1, suma_plik_1, dlugosc_plik_1); ///srednia liczb pierwszego pliku
-    sr_y = srednia_ciagu(plik_2, suma_plik_2, dlugosc_plik_2); ///srednia liczb drugiego pliku
+    sr_x = srednia_ciagu(plik_1, suma_plik_1, dlugosc_plik_1); 
+    sr_y = srednia_ciagu(plik_2, suma_plik_2, dlugosc_plik_2);
 
-    if(dlugosc_plik_1 > dlugosc_plik_2) {           ///jezeli jakis ciag jest dluzszy to srednia krotszego jest ponownie liczona
-        sr_y = suma_plik_2/dlugosc_plik_1;          ///przez podzielenie sumy wszsytkich liczb w ciagu przez liczbe znakow w dluzszym ciagu
+    if(dlugosc_plik_1 > dlugosc_plik_2) {         
+        sr_y = suma_plik_2/dlugosc_plik_1;        
     } else if(dlugosc_plik_1 < dlugosc_plik_2){
         sr_x = suma_plik_1/dlugosc_plik_2;
     }
 
-    plik_1.clear();             ///przejscie do poczatku pliku
+    plik_1.clear();           
     plik_1.seekg(0, ios::beg);
-    plik_2.clear();             ///przejscie do poczatku pliku
+    plik_2.clear();             
     plik_2.seekg(0, ios::beg);
     do {
-        ile_znakow_x = utworzenie_tablicy_ascii(plik_1, x); ///utworzenie tablicy ascii ze znakow w pliku
+        ile_znakow_x = utworzenie_tablicy_ascii(plik_1, x); 
         ile_znakow_y = utworzenie_tablicy_ascii(plik_2, y);
-        if(ile_znakow_x < ile_znakow_y) {                   ///jezeli jakis ciag jest dluzszy to krotszy jest zwiekszany przy pomocy zer
+        if(ile_znakow_x < ile_znakow_y) {                   
             for(int i=ile_znakow_x; i<ile_znakow_y; i++) x[i] = 0;
             ile_znakow_x = ile_znakow_y;
         } else {
             for(int i=ile_znakow_y; i<ile_znakow_x; i++) y[i] = 0;
             ile_znakow_y = ile_znakow_x;
         }
-        licznik += licz_licznik(x, y, sr_x, sr_y, ile_znakow_x); ///obliczenie licznika
-        mianownik_1 += licz_mianownik(x, sr_x, ile_znakow_x);   ///obliczenie pierwszej czesci mianownika
-        mianownik_2 += licz_mianownik(y, sr_y, ile_znakow_y);   ///obliczenie drugiej czesci mianownika
-    } while(ile_znakow_x == MAX_ROZMIAR); ///jezeli pilk jest dluzszy niz maksymalny rozmiar to wszytujemy kolejne czesci pliku
+        licznik += licz_licznik(x, y, sr_x, sr_y, ile_znakow_x); 
+        mianownik_1 += licz_mianownik(x, sr_x, ile_znakow_x);   
+        mianownik_2 += licz_mianownik(y, sr_y, ile_znakow_y);  
+    } while(ile_znakow_x == MAX_ROZMIAR); 
 
     wspolczynnik = wspolczynnik_korelacji(licznik, mianownik_1, mianownik_2);
     return wspolczynnik;
 }
 
-bool czy_istnieje(ifstream& plik) { ///sprawdzanie czy plik istnieje, jezeli tak to zwraca true a jezeli nie to prosi o ponowne podanie nazwy pliku
+bool czy_istnieje(ifstream& plik) { 
     char doc[MAX_ROZMIAR];
     if(plik.fail()) {
         plik.close();
